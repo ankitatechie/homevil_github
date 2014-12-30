dbdaa7b3efb3a961ac2cfbe446b87f0cd88a4427
ankitatechie <ankita.tech011@gmail.com>
2014-12-30 21:20:45 +0530

    fifth commit

--- js/booking_hours.js
+++ js/booking_hours.js
@@ -1,4 +1,4 @@
-var total_hours;
+var total_hours = 1;
 var extra_hours = 0;
 function addHours(){
 	var bedroom_hrs = document.getElementById('bedrooms').value;
@@ -9,19 +9,65 @@ function addHours(){
 		total_hours = parseFloat(bedroom_hrs) + parseFloat(bathroom_hrs);
 	}
 	document.getElementById("total_hours").innerHTML  = total_hours;
+	document.getElementById("num_hours_dropdown_front").value = total_hours;
+	ruppee_hour();
+	document.getElementById("total_hours_summary").innerHTML = parseFloat(total_hours) +" Hour(s)";
+	document.getElementById("total_ruppee_summary").innerHTML = "Rs."+ total_hours * 200 +"/- Only";
 	return total_hours;
+}
+function ruppee_hour(){
+	
+	var no_of_bedroom_hours = document.getElementById("bedrooms").value;
+	var no_of_bathroom_hours = document.getElementById("bathrooms").value;
+	if(no_of_bedroom_hours == "0.5"){
+		document.getElementById("no_bedroom").innerHTML = parseFloat(no_of_bedroom_hours) + 0.5 + " Bedroom(s)";
+	}else if(no_of_bedroom_hours == "1.5"){
+		document.getElementById("no_bedroom").innerHTML = parseFloat(no_of_bedroom_hours) + 1.5 + " Bedroom(s)";
+	}else if(no_of_bedroom_hours == "2.5"){
+		document.getElementById("no_bedroom").innerHTML = parseFloat(no_of_bedroom_hours) + 2.5 + " Bedroom(s)";
+	}else if(no_of_bedroom_hours == "1"){
+		document.getElementById("no_bedroom").innerHTML = parseFloat(no_of_bedroom_hours) + 1 + " Bedroom(s)";
+	}else if(no_of_bedroom_hours == "2"){
+		document.getElementById("no_bedroom").innerHTML = parseFloat(no_of_bedroom_hours) + 2 + " Bedroom(s)";
+	}else{
+		document.getElementById("no_bedroom").innerHTML = parseFloat(no_of_bedroom_hours) + " Bedroom(s)";
+	}
+	if(no_of_bathroom_hours == "0.5"){
+		document.getElementById("no_bathroom").innerHTML = parseFloat(no_of_bathroom_hours) + 0.5 + " Bedroom(s)";
+	}else if(no_of_bathroom_hours == "1.5"){
+		document.getElementById("no_bathroom").innerHTML = parseFloat(no_of_bathroom_hours) + 1.5 + " Bedroom(s)";
+	}else if(no_of_bathroom_hours == "2.5"){
+		document.getElementById("no_bathroom").innerHTML = parseFloat(no_of_bathroom_hours) + 2.5 + " Bedroom(s)";
+	}else if(no_of_bathroom_hours == "1"){
+		document.getElementById("no_bathroom").innerHTML = parseFloat(no_of_bathroom_hours) + 1 + " Bedroom(s)";
+	}else if(no_of_bathroom_hours == "2"){
+		document.getElementById("no_bathroom").innerHTML = parseFloat(no_of_bathroom_hours) + 2 + " Bedroom(s)";
+	}else{
+		document.getElementById("no_bathroom").innerHTML = parseFloat(no_of_bathroom_hours) + " Bedroom(s)";
+	}
+	document.getElementById("bedroom_hours").innerHTML = "Approx. "+ parseFloat(no_of_bedroom_hours)+ " Hour(s)";
+	document.getElementById("bathroom_hours").innerHTML = "Approx. "+ parseFloat(no_of_bathroom_hours)+ " Hour(s)";
+	document.getElementById("extra_work_hours").innerHTML = extra_hours +" Hour(s)";
 }
 function addExtraHours(){
 	var total_hours = addHours();
 	total_hours = total_hours + 0.5;
 	extra_hours = extra_hours + 0.5;
 	document.getElementById("total_hours").innerHTML  = total_hours;
+	document.getElementById("num_hours_dropdown_front").value = total_hours;
+	ruppee_hour();
+	document.getElementById("total_hours_summary").innerHTML = total_hours +" Hour(s)";
+	document.getElementById("total_ruppee_summary").innerHTML = "Rs."+ total_hours * 200 +"/- Only";
 }
 function subtractHours(){
 	var new_hours = addHours();
 	total_hours = new_hours - 0.5;
 	extra_hours = extra_hours - 0.5;
 	document.getElementById("total_hours").innerHTML  = total_hours;
+	document.getElementById("num_hours_dropdown_front").value = total_hours;
+	ruppee_hour();
+	document.getElementById("total_hours_summary").innerHTML = total_hours +" Hour(s)";
+	document.getElementById("total_ruppee_summary").innerHTML = "Rs."+ total_hours * 200 +"/- Only";
 }
 function laundryVisibility(){
 	var image = document.getElementById("img-laundry");
@@ -115,13 +161,67 @@ function windowsVisibility(){
 	}
 }
 
-		function showContactDetails(){
-			document.getElementById("contact_details").style.display = 'block';
-			document.getElementById("about_home").style.display = 'none';
-			document.getElementById("calendar_section").style.display = 'none';
-		}
-		function showCalendarSection(){
-			document.getElementById("contact_details").style.display = 'none';
-			document.getElementById("about_home").style.display = 'none';
-			document.getElementById("calendar_section").style.display = 'block';
-		}
+function minReqHours(){
+	if(document.getElementById("num_hours_dropdown_front").value < (total_hours + 0.5)){
+		document.getElementById("customer-note-control-front").style.display = "";
+	}else{
+		document.getElementById("customer-note-control-front").style.display = "none";
+	}
+}
+
+function showContactDetails(){
+if(document.getElementById("date").value=="" || document.getElementById("time").value==""){
+	document.getElementById("calendar_error").style.display="block";
+	document.getElementById("calendar_section").style.borderColor = "red";
+}else{
+	document.getElementById("contact_details").style.display = 'block';
+	document.getElementById("about_home").style.display = 'none';
+	document.getElementById("calendar_section").style.display = 'none';
+	document.getElementById("end_section").style.display = 'none';
+	document.getElementById("availability-pane-icon").classList.remove( "active" );
+	document.getElementById("availability-pane-icon").classList.add( "access-approved" );
+	document.getElementById("contact-pane-icon").classList.add( "active" );
+	}
+}
+function showCalendarSection(){
+	if(total_hours > 0){
+	document.getElementById("contact_details").style.display = 'none';
+	document.getElementById("about_home").style.display = 'none';
+	document.getElementById("calendar_section").style.display = 'block';
+	document.getElementById("end_section").style.display = 'none';
+	document.getElementById("availability-pane-icon").classList.add( "active" );
+	document.getElementById("info-pane-person-icon").classList.remove( "active" );
+	}else{
+	document.getElementById("error").style.display="block";
+	document.getElementById("about_home").style.borderColor = "red";
+	}
+	
+}
+function showSummary(){
+	document.getElementById("contact_details").style.display = 'none';
+	document.getElementById("about_home").style.display = 'none';
+	document.getElementById("calendar_section").style.display = 'none';
+	document.getElementById("end_section").style.display = 'block';
+	/* Display Address */
+	var address1 = document.getElementById("address_street1").value;
+	var address2 = document.getElementById("address_street2").value;
+	var city = document.getElementById("city").value;
+	document.getElementById("display_address").innerHTML = address1 + "," + address2;
+	document.getElementById("display_city").innerHTML = city;
+	/* Display Contact Info */
+	var name = document.getElementById("name").value;
+	var phone = document.getElementById("phone").value;
+	var email = document.getElementById("email").value;
+	document.getElementById("display_name").innerHTML = name;
+	document.getElementById("display_phone").innerHTML = phone;
+	document.getElementById("display_email").innerHTML = email;
+	/* Display Time Info */
+	var date = document.getElementById("date").value;
+	var time = document.getElementById("time").value;
+	document.getElementById("cleaning_date").innerHTML = date;
+	document.getElementById("cleaning_time").innerHTML = time;
+	
+	document.getElementById("contact-pane-icon").classList.remove( "active" );
+	document.getElementById("contact-pane-icon").classList.add( "access-approved" );
+	document.getElementById("review-order-pane-icon").classList.add( "active" );
+}
